ae187680fd1284180e98868af9ca8fac2638d996
williamnijsse <34302171+williamnijsse@users.noreply.github.com>
2018-09-27 08:52:19 +0200

    opdracht over functions gemaakt

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 //les variables en types
+console.log("les over variables en types");
 
 const number = '4';
 
@@ -25,6 +26,7 @@ else{
 //je kunt ze niet goed vergelijken, omdat a een number is en b een string.
 
 //les conditionals begint vanaf hier
+console.log("les over conditionals");
 
 let cijfer = 5;
 
@@ -73,6 +75,9 @@ console.log(tekst);
 
 
 // les over loops
+console.log("les over loop");
+
+// opdracht 1
 let x = 1
 
 while(x <= 20) {
@@ -87,6 +92,8 @@ while(x <= 20) {
     x = x+1
 }
 
+//opdracht 2
+
 let firstNumber = 0
 let secondNumber = 1
 let firstAndSecond = firstNumber + secondNumber
@@ -98,6 +105,8 @@ while(firstAndSecond <1000) {
     firstAndSecond = firstNumber + secondNumber;
 }
 
+//opdracht 3
+
 let Getallen = [2, 4, 8, 9, 12, 13];
 let totalOfAll = 0;
 
@@ -107,3 +116,58 @@ for(z=0; z<Getallen.length; z++) {
 }
 console.log(totalOfAll);
 
+//les over functions
+
+//opdracht 1
+console.log("les over functions");
+
+function fibonacci() {
+let Getallen = [2, 4, 8, 9, 12, 13];
+let totalOfAll = 0;
+
+for(z=0; z<Getallen.length; z++) {
+    console.log(Getallen[z]);
+    totalOfAll = totalOfAll + Getallen[z];
+}
+console.log(totalOfAll);
+}
+
+console.log(fibonacci());
+
+//opdracht 2
+
+function countdown(jaartal){
+    var number = 10;
+    while(number >= 1){
+        console.log(number);
+        number--;
+        if(number == 0){
+            console.log("happy",jaartal);
+        }
+    }
+}
+
+console.log(countdown("2019"));
+
+//opdracht 3
+function declare() {
+    a = 2;
+    b = 3;
+    console.log(a*b);
+    var a;
+    var b;
+}
+
+console.log(declare());
+
+function expression() {
+    var x = 20;
+    var string = "hallo";
+    var som = x + z;
+    console.log(string + stringTwo);
+    
+    var z = 3;
+    var stringTwo = "william";
+}
+
+console.log(expression());
