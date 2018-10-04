4f87fac9c507cfa187ef065c3f8450e2e22ea2a8
williamnijsse <34302171+williamnijsse@users.noreply.github.com>
2018-10-04 09:49:07 +0200

    opdracht 1 en 2 gemaakt en alles in aparte bestanden gezet

---
+++ assets/arrays.js
@@ -0,0 +1,34 @@
+//Opdracht 1: random element uit array
+
+const lapRounds = [2.99,  3.00, 3.01, 4.01, 2.79, 2.88, 3.10, 4.12]; 
+let randomNumber = lapRounds[Math.floor(lapRounds.length * Math.random())];
+console.log(randomNumber);
+
+//opdracht 2: 
+const allMyRecords = [
+    ["The Who - Pinball Wizard", "Rolling Stones - Exile on main street", "Police - Message in a bottle"],
+    ["De Dijk - Alle 40 Goed", "Het Goede Doel - Belgie", "Doe Maar - skunk"]
+ ];
+
+console.table(allMyRecords);
+
+for (let i = 0; i < allMyRecords.length; i++) {
+    for (let d = 0; d < allMyRecords[i].length; d++) {
+        console.log(allMyRecords[i][d])    
+    }
+}
+
+//opdracht 3: Gebruik de .filter functie uit javascript om precies hetzelfde resultaat te behalen als de for-loop. Geef aan waarom de .filter functie fijner/beter werkt.
+
+const filteredLapRoundsWithForLoop = function () {
+    let newArray = [];
+    for (let i = 0; i < lapRounds.length; i++) {
+ 
+        if (lapRounds[i] < 4) {
+            newArray.push(lapRounds[i]);
+        }
+    }
+    return newArray;
+}
+
+console.log(filteredLapRoundsWithForLoop(lapRounds));
